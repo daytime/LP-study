6edc19226e76cf0eedeb8cce6463096f00090f9e
ikeda <tam@archway.ddo.jp>
2023-01-16 00:04:54 +0900

    ある程度できてきた。リファクタリングする前。44:45

--- tabCreate/tab.js
+++ tabCreate/tab.js
@@ -1,1 +1,42 @@
-console.log('Hello World!');
+
+(() => {
+
+const $doc = document;
+const $tab = $doc.getElementById('js-tab');
+const $nav = $tab.querySelectorAll('[data-nav]');
+const $content = $tab.querySelectorAll('[data-content]');
+
+const init = () => {
+$content[0].style.display = 'block';
+};
+init();
+
+//クリックしたら起こるイベント
+const handleClick = (e) => {
+    e.preventDefault();
+    // console.log('e',e);
+//クリックされたnavとそのdataを取得
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
+
+    console.log('targetVal', targetVal);
+    //対象のコンテンツをアクティブ化する
+    // $tac.querySelectorAll(`[data-content="${targetVal}"]`) //スニペットで生成された書き方
+    $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+    $nav[targetVal].classList.add('is-active');
+    console.log('$nav[targetVal].classList', $nav[targetVal].classList);
+
+};
+
+//全nav要素に対して関数を適応・発火
+let index = 0;
+while (index < $nav.length){
+    $nav[index].addEventListener('click', (e) => handleClick(e));
+    index++;
+}
+
+// $nav[0].addEventListener('click', (e) => handlerClick(e));
+
+
+
+})();
